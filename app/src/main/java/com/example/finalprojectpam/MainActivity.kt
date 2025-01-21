12d2a26c1d05c326fbd8eee73ea8cd9033a6fd3b
irajesicajanuaris <jjiraaa1@gmail.com>
2025-01-21 22:29:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalprojectpam/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.finalprojectpam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finalprojectpam.ui.navigation.PengelolaHalaman
+import com.example.finalprojectpam.ui.theme.FinalProjectPAMTheme
+import com.example.finalprojectpam.ui.view.event.HomeEventScreen
+import com.example.finalprojectpam.ui.viewmodel.EventApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinalProjectPAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FinalProjectPAMTheme {
+        Greeting("Android")
+    }
+}
