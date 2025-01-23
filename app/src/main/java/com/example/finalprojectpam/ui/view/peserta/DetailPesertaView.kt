f6defc0421c316422d910eaffef8a068c6b70d45
irajesicajanuaris <jjiraaa1@gmail.com>
2025-01-23 17:44:25 +0700

    menambahkan DetailPesertaView

---
+++ app/src/main/java/com/example/finalprojectpam/ui/view/peserta/DetailPesertaView.kt
@@ -0,0 +1,125 @@
+package com.example.finalprojectpam.ui.view.peserta
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojectpam.ui.navigation.DestinasiNavigasi
+import com.example.finalprojectpam.ui.viewmodel.PenyediaViewModel
+import com.example.finalprojectpam.ui.viewmodel.Peserta.DetailPesertaViewModel
+
+
+object DestinasiDetailPeserta : DestinasiNavigasi {
+    override val route = "detail_peserta/{id_peserta}"
+    override val titleRes = "Detail Peserta"
+    const val Id_peserta = "id_peserta"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPesertaScreen(
+    onEditClick: (Int) -> Unit = { },
+    onBackClick: () -> Unit = { },
+    modifier: Modifier = Modifier,
+    viewModel: DetailPesertaViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    idPeserta: Int
+) {
+    val peserta = viewModel.PesertauiState.detailPesertaUiEvent
+
+    LaunchedEffect(idPeserta) {
+        viewModel.fetchDetailPeserta(idPeserta)
+    }
+    val isLoading = viewModel.PesertauiState.isLoading
+    val isError = viewModel.PesertauiState.isError
+    val errorMessage = viewModel.PesertauiState.errorMessage
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Detail Peserta") },
+                navigationIcon = {
+                    IconButton(onClick = { onBackClick() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onEditClick(peserta.id_peserta) },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Filled.Edit, contentDescription = "Edit Peserta")
+            }
+        },
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                if (isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                else if (isError) {
+                    Text(
+                        text = errorMessage,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                else if (viewModel.PesertauiState.isUiPesertaNotEmpty) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Card(
+                            modifier = Modifier.fillMaxWidth(),
+                            elevation = CardDefaults.cardElevation(8.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(16.dp),
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                Text(text = "Id Peserta: ${peserta.id_peserta}", style = MaterialTheme.typography.bodyLarge)
+                                Text(text = "Nama Peserta: ${peserta.nama_peserta}", style = MaterialTheme.typography.bodyLarge)
+                                Text(text = "Email: ${peserta.email}", style = MaterialTheme.typography.bodyLarge)
+                                Text(text = "Nomor Telepon: ${peserta.nomor_telepon}", style = MaterialTheme.typography.bodyLarge)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
