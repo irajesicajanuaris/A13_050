c40d0386317c07845f27d8155614c37bce5e010f
irajesicajanuaris <jjiraaa1@gmail.com>
2025-01-23 13:24:13 +0700

    menambahkan PesertaService

---
+++ app/src/main/java/com/example/finalprojectpam/service_api/PesertaService.kt
@@ -0,0 +1,39 @@
+package com.example.finalprojectpam.service_api
+
+import com.example.finalprojectpam.model.AllPesertaResponse
+import com.example.finalprojectpam.model.Event
+import com.example.finalprojectpam.model.Peserta
+import com.example.finalprojectpam.model.PesertaDetailResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+
+interface PesertaService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @POST("peserta/store")
+    suspend fun insertPeserta(@Body peserta: Peserta)
+
+    @GET("peserta")
+    suspend fun getAllPeserta(): AllPesertaResponse
+
+    @GET("peserta/{id_peserta}")
+    suspend fun getPesertabyidPeserta(@Path("id_peserta") id_peserta: Int): PesertaDetailResponse
+
+    @PUT("peserta/{id_peserta}")
+    suspend fun updatePeserta(@Path("id_peserta") id_peserta: Int, @Body peserta: Peserta)
+
+    @DELETE("peserta/{id_peserta}")
+    suspend fun deletePeserta(@Path("id_peserta") id_peserta: Int): Response<Void>
+
+}
