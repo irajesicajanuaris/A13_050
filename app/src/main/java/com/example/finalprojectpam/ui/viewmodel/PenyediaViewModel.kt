34f6c8c1036e85ca127da366a76377990959be25
irajesicajanuaris <jjiraaa1@gmail.com>
2025-01-28 12:08:41 +0700

    mempeprbarui PenyediaViewModel

--- app/src/main/java/com/example/finalprojectpam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/viewmodel/PenyediaViewModel.kt
@@ -6,12 +6,24 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.finalprojectpam.EventApplications
-import com.example.finalprojectpam.ui.view.event.DetailEventScreen
+import com.example.finalprojectpam.ui.viewmodel.Peserta.DetailPesertaViewModel
 import com.example.finalprojectpam.ui.viewmodel.Peserta.HomePesertaViewModel
+import com.example.finalprojectpam.ui.viewmodel.Peserta.InsertPesertaViewModel
+import com.example.finalprojectpam.ui.viewmodel.Peserta.UpdatePesertaViewModel
 import com.example.finalprojectpam.ui.viewmodel.event.DetailEventViewModel
 import com.example.finalprojectpam.ui.viewmodel.event.HomeEventViewModel
 import com.example.finalprojectpam.ui.viewmodel.event.InsertEventViewModel
 import com.example.finalprojectpam.ui.viewmodel.event.UpdateEventViewModel
+import com.example.finalprojectpam.ui.viewmodel.tiket.DetailTiketViewModel
+//import com.example.finalprojectpam.ui.viewmodel.tiket.DetailTiketViewModel
+import com.example.finalprojectpam.ui.viewmodel.tiket.HomeTiketViewModel
+import com.example.finalprojectpam.ui.viewmodel.tiket.InsertTiketViewModel
+import com.example.finalprojectpam.ui.viewmodel.tiket.UpdateTiketViewModel
+import com.example.finalprojectpam.ui.viewmodel.transaksi.DetailTransaksiViewModel
+//import com.example.finalprojectpam.ui.viewmodel.tiket.UpdateTiketViewModel
+import com.example.finalprojectpam.ui.viewmodel.transaksi.HomeTransaksiViewModel
+import com.example.finalprojectpam.ui.viewmodel.transaksi.InsertTransaksiViewModel
+import com.example.finalprojectpam.ui.viewmodel.transaksi.UpdateTransaksiViewModel
 
 
 object PenyediaViewModel{
@@ -20,7 +32,23 @@ object PenyediaViewModel{
         initializer { InsertEventViewModel(aplikasiEvent().container.eventRepository) }
         initializer { DetailEventViewModel(aplikasiEvent().container.eventRepository) }
         initializer { UpdateEventViewModel(createSavedStateHandle(),aplikasiEvent().container.eventRepository) }
+
         initializer { HomePesertaViewModel(aplikasiEvent().container.pesertaRepository) }
+        initializer { InsertPesertaViewModel(aplikasiEvent().container.pesertaRepository) }
+        initializer { DetailPesertaViewModel(aplikasiEvent().container.pesertaRepository) }
+        initializer { UpdatePesertaViewModel(createSavedStateHandle(),aplikasiEvent().container.pesertaRepository) }
+
+        initializer { HomeTiketViewModel(aplikasiEvent().container.tiketRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+        initializer { InsertTiketViewModel(aplikasiEvent().container.tiketRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+        initializer { DetailTiketViewModel(aplikasiEvent().container.tiketRepository) }
+        initializer { UpdateTiketViewModel(createSavedStateHandle(),aplikasiEvent().container.tiketRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+
+        initializer { HomeTransaksiViewModel(aplikasiEvent().container.transaksiRepository,aplikasiEvent().container.tiketRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+        initializer { InsertTransaksiViewModel(aplikasiEvent().container.transaksiRepository, aplikasiEvent().container.tiketRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+        initializer { DetailTransaksiViewModel(aplikasiEvent().container.transaksiRepository,aplikasiEvent().container.eventRepository,aplikasiEvent().container.pesertaRepository) }
+        initializer { UpdateTransaksiViewModel(createSavedStateHandle(),aplikasiEvent().container.transaksiRepository,aplikasiEvent().container.tiketRepository) }
+
+
     }
 }
 fun CreationExtras.aplikasiEvent(): EventApplications =
